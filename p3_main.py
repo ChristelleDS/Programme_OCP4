46b331993bb1f705f0739d5ebf3eb23dda149a11
ChristelleDS <christelle.dasilva75@gmail.com>
2022-03-29 09:26:53 +0200

    commentaire

--- p3_main.py
+++ p3_main.py
@@ -9,16 +9,22 @@ class Tournoi:
         self.timecontrol = timecontrol
         self.description = description
         self.nbtours = nbtours
-
     def addJoueur(self, joueur):
         self.joueurs.append(joueur)
-
+    def addTour(self, tour):
+        self.tours.append(tour)
     def saveTournoi(self):
-        return "Sauvegarde a coder"
+        return "SaveTournoi?"
 
 class Tour:
-    def __init__(self):
-        print('a coder')
+    def __init__(self, nom, dateHeureDebut, dateHeureFin, etat):
+        self.nom = nom
+        self.dateHeureDebut = dateHeureDebut
+        self.dateHeureFin = dateHeureFin
+        self.etat = etat
+        self.matchs = []
+    def addMatch(selfself,match):
+        self.matchs.append(match)
 
 class Joueur:
     def __init__(self, nom, prenom, naissance, sexe, classement):
@@ -27,7 +33,6 @@ class Joueur:
         self.naissance = naissance
         self.sexe = sexe
         self.classement = classement
-
     def majClassement(self, classement):
         self.classement = classement
 
@@ -39,7 +44,6 @@ class Match:
         self.match = match
         self.score1 = score1
         self.score2 = score2
-
     def saveScore(self):
         self.score1 = input("Score du joueur "+self.joueur1+" ?")
         resultat1 = [self.joueur1, self.score1]
@@ -47,8 +51,9 @@ class Match:
         resultat2 = [self.joueur2, self.score2]
         self.match = (resultat1, resultat2)
         print(self.match)
-        self.calculPoints()
+      #  self.calculPoints()
 
+"""
     def calculPoints(self, score1, score2, joueur1, joueur2):
         if self.score1 := self.score2:
             joueur1.classement = joueur1.classement+0.5
@@ -57,4 +62,15 @@ class Match:
             joueur1.classement = joueur1.classement+1
         elif self.score2 > self.score1:
             joueur2.classement = joueur2.classement+1
+"""
+
+tournoiParis = Tournoi("Tournoi de Paris","Paris","10/03/2022","10/03/2022","Blitz","description")
+roidesEchecs = Joueur("Roi", "Echecs", "01/08/1986", "Homme", 1)
+challenger = Joueur("Alex", "Challenger", "01/12/1995", "Homme", 2)
+julie = Joueur("Julie","Test","15/08/1975","Femme",3)
+henri = Joueur("Henri","Test","25/08/1965","Homme",4)
+john = Joueur("John","Do","25/08/1985","Homme",5)
+victor = Joueur("Victor","Do","14/07/1968","Homme",6)
+junior = Joueur("Junior","Dupont","04/04/1998","Homme",7)
+alice = Joueur("Alice","Vendi","07/10/1995","Femme",8)
 
