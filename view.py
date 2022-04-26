fea8e089018b8267b2580a14b5ef924549a8c6e7
ChristelleDS <christelle.dasilva75@gmail.com>
2022-04-26 15:36:08 +0200

    maj view

--- view.py
+++ view.py
@@ -1,4 +1,16 @@
 class Menu:
-    def methode(self):
+    def home(self):
+        message = """
+            Bienvenue\n
+            Voici les actions possibles depuis ce menu : \n
+            [1]: Créer un nouveau tournoi\n
+            [2]: Inscrire un joueur au tournoi\n
+            [3]: Démarrer un tour\n
+            [4]: Entrer les résultats du tour\n
+            [5]: Terminer le tournoi\n
+            [6]: Maj le classement d''un joueur\n
+            [7]: Accés aux reports\n
+            [X]: Sortir de l''application\n
+            """
         pass
 
